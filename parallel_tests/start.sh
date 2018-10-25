5b5dd3d98ded7ea7f73255ec825d3fb9c76e8c92
Kory Draughn <korydraughn@renci.org>
2018-10-25 13:58:59 -0400

    Small changes. Runs all tests in parallel now.

--- parallel_tests/start.sh
+++ parallel_tests/start.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
+test_name=$1
+sleep_time=${2:-60}
+
 # Start the Postgres database.
 service postgresql start
-sleep ${2:-60}
+sleep $sleep_time
 
 # Set up iRODS.
 python /var/lib/irods/scripts/setup_irods.py < /var/lib/irods/packaging/localhost_setup_postgres.input
@@ -10,7 +13,9 @@ python /var/lib/irods/scripts/setup_irods.py < /var/lib/irods/packaging/localhos
 # Run test.
 cp /run_test.sh /var/lib/irods/
 chown irods:irods /var/lib/irods/run_test.sh
-su - irods -c "./run_test.sh $1"
+su - irods -c "./run_test.sh $test_name"
 
-# Keep container running.
-tail -f /dev/null
+# Keep container running if the test fails.
+if [[ $? != 0 ]]; then
+    tail -f /dev/null
+fi
