0bc8abe33feaca25d7615ca310016e9efb18549a
Kory Draughn <korydraughn@renci.org>
2018-10-25 12:15:21 -0400

    Converted dockerfile to multi-stage process.

--- parallel_tests/build_image.sh
+++ parallel_tests/build_image.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker build -t irods_tests .
+docker build -t irods_test_env .
--- parallel_tests/run_tests_in_parallel.sh
+++ parallel_tests/run_tests_in_parallel.sh
@@ -1,4 +1,62 @@
 #! /bin/bash
 
-docker run -d --rm --name itrim irods_tests test_itrim.Test_Itrim
-docker run -d --rm --name irm irods_tests test_irm.Test_Irm
+for name in test_ichksum \
+            test_ichmod \
+            test_ils \
+            test_irepl \
+            test_irm \
+            test_irmdir \
+            test_itrim
+do
+    docker run -d --rm --name $name irods_test_env $name
+done
+
+exit 
+
+# This is the 2nd test.
+            #test_auth \
+for name in test_all_rules \
+            test_catalog \
+            test_chunkydevtest \
+            test_client_hints \
+            test_compatibility \
+            test_control_plane \
+            test_faulty_filesystem \
+            test_federation \
+            test_iadmin \
+            test_ichksum \
+            test_ichmod \
+            test_icommands_file_operations \
+            test_igroupadmin \
+            test_ils \
+            test_imeta_help \
+            test_imeta_set \
+            test_ipasswd \
+            test_iphymv \
+            test_iput_options \
+            test_iquest \
+            test_ireg \
+            test_irepl \
+            test_irm \
+            test_irmdir \
+            test_irodsctl \
+            test_irsync \
+            test_iscan \
+            test_iticket \
+            test_itrim \
+            test_load_balanced_suite \
+            test_log_levels \
+            test_native_rule_engine_plugin \
+            test_quotas \
+            test_resource_configuration \
+            test_resource_tree \
+            test_resource_types \
+            test_rule_engine_plugin_framework \
+            test_rulebase \
+            test_ssl \
+            test_symlink_operations \
+            test_util \
+            test_xmsg
+do
+    docker run -d --rm --name $name irods_test_env $name
+done
