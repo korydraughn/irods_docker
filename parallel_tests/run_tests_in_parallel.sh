61fe5fe5c2f0ddbc6cfef9df515830fa76362236
Kory Draughn <korydraughn@renci.org>
2018-10-26 11:26:14 -0400

    Updated test list to match the python test suite list.

--- parallel_tests/run_tests_in_parallel.sh
+++ parallel_tests/run_tests_in_parallel.sh
@@ -1,27 +1,21 @@
 #! /bin/bash
 
 test_logs_home=${1:-"$HOME/irods_test_env"}
+test_name_prefix=${2:-""}
+
 [ ! -d $test_logs_home ] && mkdir $test_logs_home
 
 for name in test_all_rules \
-            test_auth \
             test_catalog \
-            test_chunkydevtest \
-            test_client_hints \
-            test_compatibility \
             test_control_plane \
-            test_faulty_filesystem \
-            test_federation \
             test_iadmin \
             test_ichksum \
             test_ichmod \
             test_icommands_file_operations \
-            test_igroupadmin \
             test_ils \
             test_imeta_help \
             test_imeta_set \
             test_ipasswd \
-            test_iphymv \
             test_iput_options \
             test_iquest \
             test_ireg \
@@ -34,19 +28,15 @@ for name in test_all_rules \
             test_iticket \
             test_itrim \
             test_load_balanced_suite \
-            test_log_levels \
             test_native_rule_engine_plugin \
             test_quotas \
             test_resource_configuration \
             test_resource_tree \
             test_resource_types \
-            test_rule_engine_plugin_framework \
             test_rulebase \
             test_ssl \
-            test_symlink_operations \
-            test_util \
-            test_xmsg
+            test_symlink_operations
 do
-    docker run -d --rm --name $name -v $test_logs_home:/irods_test_env irods_test_env $name
+    docker run -d --rm --name ${test_name_prefix}_${name} -v $test_logs_home:/irods_test_env irods_test_env $name
 done
 
