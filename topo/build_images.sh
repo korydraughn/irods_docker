509161af74681edf6788ff579bd1ef56c8589051
Kory Draughn <korydraughn@renci.org>
2018-11-02 15:35:23 -0400

    Need to fix prefix/namespace handling for launch_topo.sh

--- topo/build_images.sh
+++ topo/build_images.sh
@@ -1,4 +1,16 @@
 #! /bin/bash
 
-docker build -t irods_topo:provider -f Dockerfile.provider .
-docker build -t irods_topo:consumer -f Dockerfile.consumer .
+suffix=
+if [ -n "$1" ]; then
+    suffix=_$1
+fi
+
+build_args="--build-arg irods_repo=https://github.com/korydraughn/irods"
+build_args="$build_args --build-arg irods_branch=re_plugin_issue_4144_partial"
+build_args="$build_args --build-arg icommands_repo=https://github.com/korydraughn/irods_client_icommands"
+build_args="$build_args --build-arg icommands_branch=4-2-stable"
+build_args="$build_args --build-arg cmake_path=/opt/irods-externals/cmake3.5.2-0/bin"
+build_args="$build_args --build-arg clang_path=/opt/irods-externals/clang3.8-0/bin"
+
+docker build -t irods_topo$suffix:provider -f Dockerfile.provider $build_args .
+docker build -t irods_topo$suffix:consumer -f Dockerfile.consumer $build_args .
--- topo/launch_topo.sh
+++ topo/launch_topo.sh
@@ -1,17 +1,31 @@
 #! /bin/bash
 
-# The total number of consumers to launch.
-consumers=${1:-3}
+prefix=
+consumers=3
+image_provider='irods_topo:provider'
+image_consumer='irods_topo:consumer'
+
+while [ -n "$1" ]; do
+    case "$1" in
+        --prefix)           shift; prefix=${1}_;;
+        --consumers)        shift; consumers=$1;;
+        --image_provider)   shift; image_provider=$1;;
+        --image_consumer)   shift; image_consumer=$1;;
+    esac
+    shift
+done
 
 # /etc/host is automatically handled by Docker.
 # All containers know about each other because they are on the same network.
 # They can also resolve each other's IP using the container name.
 
-docker run -d --rm --name topo_provider -h topo_provider --network topo_net irods_topo:provider
-docker network connect bridge topo_provider
+docker run -d --rm --name ${prefix}topo_provider -h topo_provider --network topo_net $image_provider
+docker network connect bridge ${prefix}topo_provider
 
 for ((i=0; i < consumers; i++)); do
     name=topo_consumer_$i
-    docker run -d --rm --name $name -h $name --network topo_net irods_topo:consumer
-    docker network connect bridge $name
+    prefixed_name=${prefix}$name
+    docker run -d --rm --name $prefixed_name -h $name --network topo_net $image_consumer
+    docker network connect bridge $prefixed_name
 done
+
