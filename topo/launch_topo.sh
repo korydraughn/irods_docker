9f26858c1578b748bac02e3c6c1aedb420b218eb
Kory Draughn <korydraughn@renci.org>
2018-11-08 09:44:05 -0500

    Fixed prefix issue.

--- topo/launch_topo.sh
+++ topo/launch_topo.sh
@@ -19,13 +19,14 @@ done
 # All containers know about each other because they are on the same network.
 # They can also resolve each other's IP using the container name.
 
-docker run -d --rm --name ${prefix}topo_provider -h topo_provider --network topo_net $image_provider
-docker network connect bridge ${prefix}topo_provider
+topo_provider=${prefix}topo_provider
+
+docker run -d --rm --name $topo_provider -h $topo_provider --network topo_net $image_provider
+docker network connect bridge $topo_provider
 
 for ((i=0; i < consumers; i++)); do
-    name=topo_consumer_$i
-    prefixed_name=${prefix}$name
-    docker run -d --rm --name $prefixed_name -h $name --network topo_net $image_consumer
-    docker network connect bridge $prefixed_name
+    name=${prefix}topo_consumer_$i
+    docker run -d --rm --name $name -h $name --network topo_net $image_consumer $topo_provider
+    docker network connect bridge $name
 done
 
