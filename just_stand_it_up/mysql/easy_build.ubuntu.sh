78758bb211b9a2f196f49c90d3f230851e2981cf
Kory Draughn <korydraughn@renci.org>
2021-05-19 14:24:02 -0400

    Fixed MySQL support.

--- just_stand_it_up/mysql/easy_build.ubuntu.sh
+++ just_stand_it_up/mysql/easy_build.ubuntu.sh
@@ -6,8 +6,8 @@ irods_sha=$3
 github_acct_icommands=$4
 icommands_sha=$5
 
-./build_image.unbuntu.sh --image-name ${image_name} \
-                         --irods-repo https://github.com/${github_acct_irods}/irods \
-                         --irods-branch ${irods_sha} \
-                         --icommands-repo https://github.com/${github_acct_icommands}/irods_client_icommands \
-                         --icommands-branch ${icommands_sha}
+./build_image.ubuntu.sh --image-name ${image_name} \
+                        --irods-repo https://github.com/${github_acct_irods}/irods \
+                        --irods-branch ${irods_sha} \
+                        --icommands-repo https://github.com/${github_acct_icommands}/irods_client_icommands \
+                        --icommands-branch ${icommands_sha}
